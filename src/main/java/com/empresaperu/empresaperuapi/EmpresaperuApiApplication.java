c9fb8e910daaca57ad4c9d3695464a96bb3976f4
Oscar Heredia <u201913874@upc.edu.pe>
2023-06-20 23:32:10 -0500

    (feat): Structure Initializr

---
+++ src/main/java/com/empresaperu/empresaperuapi/EmpresaperuApiApplication.java
@@ -0,0 +1,13 @@
+package com.empresaperu.empresaperuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaperuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaperuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresaperu/empresaperuapi/EmpresaperuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresaperu.empresaperuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaperuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
